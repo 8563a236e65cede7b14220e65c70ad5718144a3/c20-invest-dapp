08814f15df2b7994f82d26569c5e64cee216bf01
Warren Jitsing <warren.jitsing@gmail.com>
2020-09-07 10:23:40 +0200

    (Add) base proxy contract
    See: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/proxy/Proxy.sol
    
    Why: Allow contract to be upgradeable
    
    How: Smart Contract Proxy Pattern
    
    Tags: proxy upgradeable

---
+++ contracts/proxy/Proxy.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.0;
+
+abstract contract Proxy {
+    function _delegate(address implementation) internal {
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+
+            let result := delegatecall(gas(), implementation, 0, calldatasize(), 0, 0)
+
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            case 0 { revert(0, returndatasize())}
+            default { return(0, returndatasize())}
+
+        }
+    }
+}
