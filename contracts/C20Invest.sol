d9f700d9f331854111d359a9e9e5f3ebf46d447a
Warren Jitsing <warren.jitsing@gmail.com>
2020-09-08 18:51:03 +0200

    (Add) C20Invest pricing logic
    See: C20:buyTo()
    
    Why: Calculate number of tokens to transfer given the user's ether
         balance and then perform the transfer.
    
    How: The current price struct is queried from the C20 contract and then
         the same logic in C20:buyTo() is applied, namely multiply the user's
         ether balance by the numerator of the current price and divide by
         the denominator
    
    Tags: token purchase logic

--- contracts/C20Invest.sol
+++ contracts/C20Invest.sol
@@ -3,15 +3,18 @@ pragma solidity ^0.7.0;
 
 import "./c20_base/C20.sol";
 import "./access/Ownable.sol";
+import "./math/SafeMathNew.sol";
 
 
 contract C20Invest is Ownable {
+    using SafeMathNew for uint256;
+
     C20 c20;
 
-    uint MIN_INVESTMENT = 0.1 ether;
-    mapping (address => uint) public userBalances;
-    mapping (address => uint) public requestTime;
-    
+    uint256 MIN_INVESTMENT = 0.1 ether;
+    mapping (address => uint256) public userBalances;
+    mapping (address => uint256) public requestTime;
+
     constructor (address[] memory owners, address payable c20Address)
     Ownable(owners) {
         c20 = C20(c20Address);
@@ -31,13 +34,23 @@ contract C20Invest is Ownable {
     }
     
     function getTokens() external {
-        require(requestTime[msg.sender] < c20.previousUpdateTime());
-        require(userBalances[msg.sender] > 0);
-        userBalances[msg.sender] = 0;
-        uint numTokens = 0;
-        // TODO: implement pricing logic
-        c20.previousUpdateTime()
+        require(
+            requestTime[msg.sender] < c20.previousUpdateTime(),
+            "C20Invest: price has not updated yet"
+        );
+        require(
+            userBalances[msg.sender] > 0,
+            "C20Invest: user has no ether for conversion"
+        );
 
+        uint256 numTokens = 0;
+        uint256 priceNumerator;
+        uint256 priceDenominator;
+        (priceNumerator, priceDenominator) = c20.currentPrice();
+        numTokens = priceNumerator.mul(userBalances[msg.sender]);
+        numTokens = numTokens.div(priceDenominator);
+
+        userBalances[msg.sender] = 0;
         c20.transferFrom(address(this), msg.sender, numTokens);
     }
 
